9ae37c46b0214cea2664ee7e67b2bd6105a9800f
Falko Zurell <falko.zurell@ubirch.com>
2016-11-20 17:32:23 +0100

    making build.sh more generic
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
 
+
+CONTAINER_NAME="ubirch/aws-tools"
+
 if [ "${GO_PIPELINE_LABEL:-zero}" == "zero" ]; then
-  GO_PIPELINE_LABEL=latest
+  GO_PIPELINE_LABEL="latest"
 fi
 
 # build the docker container
@@ -9,24 +12,26 @@ function build_container() {
 
 
 
-    echo "Building AWS Tools container"
+    echo "Building AWS Tools container in ${PWD}"
 
-    docker build --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t ubirch/aws-tools:${GO_PIPELINE_LABEL} -f Dockerfile .
+    docker build --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    -t ${CONTAINER_NAME}:v${GO_PIPELINE_LABEL} -f Dockerfile .
 
 
     if [ $? -ne 0 ]; then
         echo "Docker build failed"
         exit 1
+    else
+        docker tag ${CONTAINER_NAME}:v${GO_PIPELINE_LABEL} ${CONTAINER_NAME}:latest
     fi
 
 }
 
 # publish the new docker container
 function publish_container() {
-  echo "Publishing Docker Container with version: ${GO_PIPELINE_LABEL}"
-  docker tag ubirch/aws-tools ubirch/aws-tools:latest
-  docker tag ubirch/aws-tools ubirch/aws-tools:v${GO_PIPELINE_LABEL}
-  docker push ubirch/aws-tools:v${GO_PIPELINE_LABEL} && docker push ubirch/aws-tools
+  echo "Publishing Docker Container ${CONTAINER_NAME} with version: ${GO_PIPELINE_LABEL}"
+  docker push ${CONTAINER_NAME}:v${GO_PIPELINE_LABEL} && docker push ${CONTAINER_NAME}
 
   if [ $? -ne 0 ]; then
     echo "Docker push faild"
