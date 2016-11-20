c8ee85f7ff854de024284cb65dacc355e2ced03f
Falko Zurell <falko.zurell@ubirch.com>
2016-11-20 12:50:18 +0100

    addining initial version of S3 Sync Script
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

---
+++ aws-s3-sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# this scripts sync a local directory with an AWS S3 bucket.
+# input local directory, AWS CREDENTIALS, S3 bucket
+
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-zero}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-zero}
+
+
+echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
+echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
+
+if [ "${AWS_SECRET_ACCESS_KEY}" == "zero" ]; then
+  echo "You must specify AWS_SECRET_ACCESS_KEY as environment variable"
+  exit 1
+fi
+
+if [ "${AWS_ACCESS_KEY_ID}" == "zero" ]; then
+  echo "You must specify AWS_ACCESS_KEY_ID as environment variable"
+  exit 1
+fi
+
+LOCALPATH=`echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1")`
+
+docker run --rm -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-zero} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-zero} -v $LOCALPATH:/build ubirch/aws-tools aws s3 sync /build $2 --dryrun --exclude ".git*" --delete
