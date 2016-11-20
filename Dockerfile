93a8d7ace1d471a88d7cddbf6e0642357c0085b0
Falko Zurell <falko.zurell@ubirch.com>
2016-11-20 13:41:42 +0100

    extending build file for Go CD builds
    Signed-off-by: Falko Zurell <falko.zurell@ubirch.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,24 @@
 FROM debian:jessie
 MAINTAINER Falko Zurell <falko.zurell@ubirch.com>
 
+# Build-time metadata as defined at http://label-schema.org
+  ARG BUILD_DATE
+  ARG VCS_REF
+  LABEL org.label-schema.build-date=$BUILD_DATE \
+        org.label-schema.docker.dockerfile="/Dockerfile" \
+        org.label-schema.license="e.g. MIT" \
+        org.label-schema.name="ubirch AWS Tools Container" \
+        org.label-schema.url="e.g. https://ubirch.com" \
+        org.label-schema.vcs-ref=$VCS_REF \
+        org.label-schema.vcs-type="Git" \
+        org.label-schema.vcs-url="https://github.com/ubirch/docker-aws-tools"
+
 LABEL description="uBirch aws tools container"
 RUN apt-get update && apt-get install python-pip -y && \
     apt-get autoclean && apt-get --purge -y autoremove && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 RUN pip install boto
+RUN pip install boto3
 RUN pip install awscli
 
 WORKDIR /opt
--- build.sh
+++ build.sh
@@ -1,3 +1,45 @@
-#!/bin/bash
+#!/bin/bash -x
 
-docker build -t ubirch/aws-tools .
+# build the docker container
+function build_container() {
+
+
+
+    echo "Building AWS Tools container"
+
+    docker build --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -t ubirch/aws-tools:v${GO_PIPELINE_LABEL} -f Dockerfile .
+
+
+    if [ $? -ne 0 ]; then
+        echo "Docker build failed"
+        exit 1
+    fi
+
+}
+
+# publish the new docker container
+function publish_container() {
+  echo "Publishing Docker Container with version: ${GO_PIPELINE_LABEL}"
+  docker push ubirch/aws-tools:v${GO_PIPELINE_LABEL} && docker push ubirch/aws-tools
+
+  if [ $? -ne 0 ]; then
+    echo "Docker push faild"
+    exit 1
+  fi
+
+}
+
+
+case "$1" in
+    build)
+        build_container
+        ;;
+    publish)
+        publish_container
+        ;;
+    *)
+        echo "Usage: $0 {build|publish}"
+        exit 1
+esac
+
+exit 0
